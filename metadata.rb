cfb1dc984c938b03306c3dad4beaa1c3f88f3dd6
Daniel Hahn <dha@betterplace.org>
2012-11-07 14:01:11 +0100

    version bump

--- metadata.rb
+++ metadata.rb
@@ -3,4 +3,4 @@ maintainer_email "developers@betterplace.org"
 license          "Gnu Public License v3"
 description      "Installs monitoring"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.0.1"
+version          "0.0.2"
